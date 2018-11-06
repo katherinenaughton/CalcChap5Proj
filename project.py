e78a52d25a8596f85bdae0e5192d39db18ce94fe
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-06 09:29:10 -0500

    Updated from Brython Server: 11/6/2018 9:29:09 AM

--- project.py
+++ project.py
@@ -1,1 +1,10 @@
+'''
+project.py
+Katie Naughton and Ella Edmonds
+'''
 
+function=input("What function would you like to analyze?")
+interval=input("What interval do you want your function to be on?")
+
+for 
+derivative= lamda f : (function(a+h) -function(a))/h
