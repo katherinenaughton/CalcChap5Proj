774e2d8a8c0f250590b1ddf5f498d911c72269f5
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-06 21:26:11 -0500

    Updated from Brython Server: 11/6/2018 9:26:10 PM

--- project.py
+++ project.py
@@ -7,7 +7,18 @@ function=input("What function would you like to analyze? ")
 a=int(input("Where do you want your interval to start? "))
 b=int(input("Where do you want your interval to end? "))
 
-coord=[]
+print(function)
+funclist=list(function)
+print(funclist)
+
+length=len(funclist)
+print(length)
+
+func2=lamda x : function
+print(func2)
+
+#for r in 
+ycoord=[]
 for x in range(a, (b+1)):
     print (function)
 
