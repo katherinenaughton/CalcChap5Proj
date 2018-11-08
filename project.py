df35d708390dfba939419cdb0a24f597ce6f1572
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-08 12:11:16 -0500

    Updated from Brython Server: 11/8/2018 12:11:16 PM

--- project.py
+++ project.py
@@ -1,27 +1,33 @@
 '''
 project.py
 Katie Naughton and Ella Edmonds
-'''
-#eval, with x predefined
 
-#def square(x):
-    #return x*x
-    #result=square(5)
+eval, with x predefined
+def square(x):
+    return x*x
+    result=square(5)
+print(square(5))
+'''
+
+
 import math
 function=input("What function would you like to analyze? ")
 a=int(input("Where do you want your interval to start? "))
 b=int(input("Where do you want your interval to end? "))
 
+x=x2
+
 print(function)
 
-def ycoord(x):
+def ycoord(x2):
     return eval(function)
-    result=ycooord(5)
+    result=ycoord(x2)
+ 
 
 ycoordlist=[]
 for x in range(a, (b+1)):
-    result=ycoord(x)
-    ycoordlist.append(result)
+    
+    ycoordlist.append(result(x))
     
 print(ycoordlist)
 
