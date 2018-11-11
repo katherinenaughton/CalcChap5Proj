9adc63829207df060f9e5641e9a87151d71f648f
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-11 15:54:54 -0500

    Updated from Brython Server: 11/11/2018 3:54:53 PM

--- project.py
+++ project.py
@@ -15,17 +15,17 @@ from math import log, log10, sqrt, log2
 function=input("What function would you like to analyze? ")
 x1=int(input("Where do you want your interval to start? "))
 x2=int(input("Where do you want your interval to end? "))
-step = float(input("What do you want the step to be? "))
+#step = float(input("What do you want the step to be? "))
 
 print(function)
 
+# y values
 ycoordlist1=[]
 for r in range(x1, (x2+1)):
     x=r+0.001
     Locfunction=function.lower()
     y=eval(Locfunction)
     ycoordlist1.append(y)
-    
 print(ycoordlist1)
 
 ycoordlist2=[]
@@ -34,20 +34,29 @@ for r in range(x1, (x2+1)):
     Locfunction=function.lower()
     y=eval(Locfunction)
     ycoordlist2.append(y)
-    
 print(ycoordlist2)
-
 
 intervalnum=len(ycoordlist1)
 print(intervalnum)
 
+# derivatives
 derivlist=[]
 for s in range(intervalnum):
     deriv  = ((ycoordlist1[s])-(ycoordlist2[s]))/(2*0.01)
     derivlist.append(deriv)
-
 print (derivlist)
 
+# extrema
+
+# increasing intervals
+
+# decreasing intervals
+
+# points of inflection
+
+# concave up intervals
+
+# concave down intervals
 
 
     
