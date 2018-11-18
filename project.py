e787df05b56d1adc271eee02b482b90ec1094c75
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-18 13:58:14 -0500

    Updated from Brython Server: 11/18/2018 1:58:13 PM

--- project.py
+++ project.py
@@ -2,36 +2,40 @@
 '''
 project.py
 Katie Naughton and Ella Edmonds
+eval, with x predefined
+def square(x):
+    return x*x
+    result=square(5)
+print(square(5))
+TO DO: 
+have figure out how to tell if extrema is abs/local or max/min
+have to figure our when decreasing or increasing interval is a union
 '''
-
-                                            #This is where we import the functions from the math library. 
 from math import sin, cos, tan, acos, asin, atan
 from math import exp, expm1, e, pi
 from math import log, log10, sqrt, log2
 from ggame import App, Color, LineStyle, Sprite
 from ggame import CircleAsset
 
-                                            #This is where the user inputs the function and intervals. 
+#inputs
 function=input("What function would you like to analyze? ")
 print("If you choose a log function, make sure your interval is within the domain :)")
-x1=int(input("Where do you want your interval to start (x value)? "))
-x2=int(input("Where do you want your interval to end (x value)? "))
+x1=int(input("Where do you want your interval to start? "))
+x2=int(input("Where do you want your interval to end? "))
 
-        
 #print(function)
 
-xcoordlist=[]                               #This prints a list of the x values. 
+xcoordlist=[]                               #x values
 for i in range(x1,x2+1):
     if i == x2:
         xcoordlist.append(i+.0)
     else:
         for m in [.0,.1,.2,.3,.4,.5,.6,.7,.8,.9]:
             #print(i+m)
-            xcoordlist.append(i+m)
+            xcoordlist.append(round((i+m),2))
 #print(xcoordlist)
-
-
     
+
 a=False
 ycoordlist=[]                               #This prints a list of the y values. 
 for r in xcoordlist:
@@ -39,16 +43,16 @@ for r in xcoordlist:
     Locfunction=function.lower()
     try:
         y=eval(Locfunction)
+
         ycoordlist.append(y)
+
     except: 
         a=True
         asymptote=r
         print("There is a vertical asymptote at x=", asymptote, " in this function!")
-#print(ycoordlist)
 
 
-                                             #This will find the y+.001 value for the symmetric difference quotient.
-ycoordlist1=[]                              
+ycoordlist1=[]                              #this will find the a+.001 for the dq
 for r in xcoordlist:
     x=r+0.001
     Locfunction=function.lower()
@@ -57,7 +61,7 @@ for r in xcoordlist:
 #print(ycoordlist1)
 
 
-ycoordlist2=[]                              #This will find the y-.001 value for the symmetric differnce quotient. 
+ycoordlist2=[]                              #this will find the a+.001 for the sdq
 for r in xcoordlist:
     x=r-0.001
     Locfunction=function.lower()
@@ -66,25 +70,24 @@ for r in xcoordlist:
 #print(ycoordlist2)
 
 
-intervalnum=len(ycoordlist1)                #This tells us how long our cordinate lists are 
-#print(intervalnum)                            #so we know how long to run the loop. 
+intervalnum=len(ycoordlist1)                #this tells us how long our cordinate lists are 
+#print(intervalnum)                              #so we know how long to run the loop
 
 
-derivlist=[]                                #This makes a list of the derivatives, and a rounded list
-derivlist1=[]                                   #of the derivatives. 
+derivlist=[]                                #here we will make a list of the derivatives
+derivlist1=[]
 for s in range(intervalnum):
     deriv  = ((ycoordlist1[s])-(ycoordlist2[s]))/(2*0.001)
     derivlist1.append(round(deriv,2))
     derivlist.append(deriv)
-#print (derivlist1)
 #print (derivlist)
 
-                                            #This makes a list of the x and y coordinates, and the list of 
-#deriv/x value/y value zip                      #corresponding derivatives. 
-xyderivzip=list(zip(xcoordlist, ycoordlist, derivlist1))
+
+#deriv/x value/y value zip
+xyderivzip=list(zip(xcoordlist, ycoordlist, derivlist))
 #print(xyderivzip)
 
-'''
+
 extremalist=[]                              #here we find where d1 = 0
 increasinglist=[]                           #here we find the interval where it inc/dec
 decreasinglist=[]
@@ -96,23 +99,23 @@ for d in xyderivzip:
     B=xyderivzip[b]
     C=xyderivzip[c]
     if d[0] == xcoordlist[0]:
-        if d[2] > 0:
+        if d[2] >= 0:
             if d[1] < C[1]:
                 print((d[0],round(d[1],2)),"is a local min")
             elif d[1] > C[1]:
                 print((d[0],round(d[1],2)),"is a local max")
-        zero.append((' ',d[0],'+'))
-        if d[2] < 0:
+            zero.append((' ',d[0],'+'))
+        if d[2] <= 0:
+            if d[1] < C[1]:
+                print((d[0],round(d[1],2)),"is a local min")
+            elif d[1] > C[1]:
+                print((d[0],round(d[1],2)),"is a local max")
+            zero.append((' ',d[0],'-'))
+    elif d[0] == xcoordlist[-1]:
+        if d[2] > 0:
             if d[1] < B[1]:
                 print((d[0],round(d[1],2)),"is a local min")
             elif d[1] > B[1]:
-                print((d[0],round(d[1],2)),"is a local max")
-        zero.append((' ',d[0],'-'))
-    elif d[0] == xcoordlist[-1]:
-        if d[2] > 0:
-            if d[1] < C[1]:
-                print((d[0],round(d[1],2)),"is a local min")
-            elif d[1] > C[1]:
                 print((d[0],round(d[1],2)),"is a local max")
             zero.append(('+',d[0],' '))
         if d[2] < 0:
@@ -147,7 +150,7 @@ for d in xyderivzip:
                 after = '+'
             if d[2] == 0:
                 zero.append((before,d[0],after))
-            elif B[2] != 0 and C[2] !=0:
+            elif B[2] != 0 and C[2] != 0:
                 zero.append((before,(B[0]+d[0])/2,after))
     b+=1
     c+=1
@@ -169,7 +172,7 @@ for d in zero:
         incstart.append(d[1])
     elif d[2] == '-':
         decstart.append(d[1])
-        
+print()
 #print(incstart)
 #print(incend)
 #print(decstart)
@@ -181,7 +184,7 @@ else:
     for d in incstart:
         m = incstart.index(d)
         print(d,"to",incend[m])
-
+print()
 if len(decstart) == 0:
     print("Your function is never decreasing.")
 else:
@@ -189,13 +192,11 @@ else:
     for d in decstart:
         m = decstart.index(d)
         print(d,"to",decend[m])    
+print()
 
-'''   
-
-                                                    # This creates list of the y+0.001 and y-0.001 values to use
-                                                        # in the symmetric differnce quotient to find the 
-                                                            #second derivatives. 
 #second derivatives
+
+
 ycoordlista=[]                              
 for r in xcoordlist:
     x=r+0.002
@@ -212,79 +213,148 @@ for r in xcoordlist:
     ycoordlistb.append(y)
 #print(ycoordlistb)
 
-derivlista=[]                                #This makes a list of the derivatives, and a rounded list                                 #of the derivatives. 
+intervalnum=len(ycoordlist1)                #This tells us how long our cordinate lists are 
+#print(intervalnum)                            #so we know how long to run the loop. 
+
+derivlist=[]                                #This makes a list of the derivatives, and a rounded list
+derivlist1=[]                                   #of the derivatives. 
+for s in range(intervalnum):
+    deriv  = ((ycoordlist1[s])-(ycoordlist2[s]))/(2*0.001)
+    derivlist1.append(round(deriv,2))
+    derivlist.append(deriv)
+#print (derivlist1)
+#print (derivlist)
+
+derivlista=[]                                #This makes a list of the derivatives, and a rounded list
+derivlista1=[]                                   #of the derivatives. 
 for s in range(intervalnum):
     deriva  = ((ycoordlista[s])-(ycoordlist[s]))/(2*0.001)
+    #derivlista1.append(round(deriva,2))
     derivlista.append(deriva)
+#print (derivlista1)
 #print (derivlista)
 
-derivlistb=[]                                #This makes a list of the derivatives, and a rounded list                                 #of the derivatives. 
+derivlistb=[]                                #This makes a list of the derivatives, and a rounded list
+derivlistb1=[]                                   #of the derivatives. 
 for s in range(intervalnum):
     derivb  = ((ycoordlist[s])-(ycoordlistb[s]))/(2*0.001)
+    #derivlistb1.append(round(derivb,2))
     derivlistb.append(derivb)
+#print (derivlistb1)
 #print (derivlistb)
-
     
 deriv2list=[]
 deriv2list1=[]
 for s in range(intervalnum):
     deriv2  = ((derivlista[s])-(derivlistb[s]))/(2*0.001)
-    deriv2list1.append(round(deriv2,2))
+    #deriv2list1.append(round(deriv2,2))
     deriv2list.append(deriv2)
+#print(deriv2list1)
 #print (deriv2list)
-print(deriv2list1)
 
-
-
-
-
-
-                                                    #This creates a list with the x and y coordinates, 
-                                                        #first derivatives and second derivatives. 
 xyderiv2zip=list(zip(xcoordlist, ycoordlist, derivlist, deriv2list))
 #print(xyderiv2zip)
 
-                                                    #These loops check to see where the second derivative is zero,
-                                                        #positive or negative in order to determine POI and concave
-                                                            #up and down intervals. 
-'''
 
-# points of inflection
-poilist=[]
-concaveuplist=[]
-concavedownlist=[]
+poilist = []                              #here we find where d2 = 0
+cculist = []                           #here we store the interval where it ccu/ccd
+ccdlist = []
+poi = []
+b = -1
+c = 1
+e=len(xyderiv2zip)
 for d in xyderiv2zip:
-    if d[3]==0:
-       poistlist.append((d[0], d[1]))
-    elif d[3]>=0:
-        concaveuplist.append(d[0])
-    elif d[3]<=0:
-        concavedownlist.append(d[0])
-    if a: 
-        print("There is a ALSO a point of inflection at x=", asymptote, ".")
-print (poilist)
-print (concaveuplist)
-print (concavedownlist)
-# concave up interval(s)
-concaveuplist=[]
-for d in xyderiv2zip:
-    if d[3]>=0:
-        concaveuplist.append(d[0])
-print (concaveuplist)
-lengthconcaveup=len(concaveuplist)
-print(lengthconcaveup)
-#print('Your function is concave up from' concaveuplist[0] 'to' concaveuplist[lengthconcaveup])
-#concave down interval(s)
-concavedownlist=[]
-for d in xyderiv2zip:
-    if d[3]<=0:
-        concavedownlist.append(d[0])
-print (concavedownlist)
-lengthconcavedown=len(concavedownlist)
-#print('Your function is concavedown from' concavedownlist[0] 'to' concavedownlist[lengthconcavedown])
+    B=xyderiv2zip[b]
+    C=xyderiv2zip[c]
+    if d[0] == xcoordlist[0]:
+        if C[3] > 0:
+            poi.append((' ',d[0],'+'))
+        if C[3] < 0:
+            poi.append((' ',d[0],'-'))
+    elif d[0] == xcoordlist[-1]:
+        if B[3] > 0:
+            poi.append(('+',d[0],' '))
+        if B[3] < 0:
+            poi.append(('-',d[0],' '))
+    else: 
+        if B[3]*d[3] > 0:
+            if d[3] > 0:
+                cculist.append(d[0])
+            elif d[3] < 0:
+                ccdlist.append(d[0]) 
+        else:
+            poilist.append((d[0], d[1]))
+            if B[3] < 0 and C[3] < 0:
+                print((d[0],round(d[1],2)),"is just a 0")
+            elif B[3] < 0 and C[2] > 0:
+                print((d[0],round(d[1],2)),"is a poi from ccd to ccu")
+                cculist.append(d[0])
+            elif B[3] > 0 and C[3] < 0:
+                print((d[0],round(d[1],2)),"is a poi from ccu to ccd")
+                ccdlist.append(d[0])
+            if B[3] < 0:
+                before = '-'
+            elif B[3] > 0:
+                before = '+'
+            if C[3] < 0:
+                after = '-'
+            elif C[3] > 0:
+                after = '+'
+            if B[3] != 0 and C[3] != 0:
+                poi.append((before,d[0],after))
+    b+=1
+    c+=1
+    if c == e:
+        c=0
 
-                                    #This is the code for the graphs of the function and derivative.
-'''                                 #This defines the colors.
+#print(poi)
+if len(poi) == 2:
+    print("There is no point of inflection")
+
+print()
+
+ccustart = []
+ccuend = []
+ccdstart = []
+ccdend = []
+
+for d in poi:
+    if d[0] == '+':
+        ccuend.append(d[1])
+    elif d[0] == '-':
+        ccdend.append(d[1])
+    if d[2] == '+':
+        ccustart.append(d[1])
+    elif d[2] == '-':
+        ccdstart.append(d[1])
+        
+#print(ccustart)
+#print(ccuend)
+#print(ccdstart)
+#print(ccdend)
+
+if len(ccustart) == 0:
+    print("your function is never ccu.")
+else:
+    print("Your function is ccu from:")
+    for d in ccustart:
+        m = ccustart.index(d)
+        print(d,"to",ccuend[m])
+
+print()
+
+if len(ccdstart) == 0:
+    print("Your function is never ccd.")
+else:
+    print("Your function is ccd from:")
+    for d in ccdstart:
+        m = ccdstart.index(d)
+        print(d,"to",ccdend[m]) 
+
+
+#graphing code
+
+
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
@@ -293,7 +363,7 @@ purple = Color(0x800080, 1.0)
 purple2 = Color(0x9932CC, 1.0)
                                     #This defines the points that will plot the function graph.
 thinline = LineStyle(1, black)
-points = CircleAsset(5, thinline, blue)
+points = CircleAsset(2, thinline, blue)
                                     #This defines the coordinates to graph the original function. 
 graphycoords=[y*-1 for y in ycoordlist]
 #print(graphycoords)
@@ -302,13 +372,15 @@ ycoords= graphycoords
 
 #print(xcoordlist)
 #print(ycoordlist)
+a = sqrt(x1**2)
+b = sqrt(x2**2)
                                     #This graphs the function. 
 xycoords=list(zip(xcoords,ycoords))
 for i in xycoords: 
-    Sprite(points, ((25*(i[0]+20),(25*(i[1]+10)))))
+    Sprite(points, ((25*(i[0]+a+2),(25*(i[1]+10)))))
 
                                     #This defines the points that will plot the graph.
-points = CircleAsset(5, thinline, purple)
+points = CircleAsset(2, thinline, purple)
                                      #This defines the coordinates to graph the derivative.
 graphy2coords=[y*-1 for y in derivlist]
 x2coords = xcoordlist
@@ -316,17 +388,17 @@ y2coords = graphy2coords
 xy2coords=list(zip(x2coords,y2coords))
                                      #This graphs the derivative.
 for i in xy2coords: 
-    Sprite(points, ((25*(i[0]+20)),(25*(i[1]+10))))
+    Sprite(points, ((25*(i[0]+a+b+4)),(25*(i[1]+10))))
     
-points = CircleAsset(5, thinline, red)
+points = CircleAsset(2, thinline, red)
                                      #This defines the coordinates to graph the derivative.
 graphy3coords=[y*-1 for y in deriv2list]
 x3coords = xcoordlist
 y3coords = graphy3coords
 xy3coords=list(zip(x3coords,y3coords))
-                                     #This graphs the derivative.
+                                     #This graphs the second derivative.
 for i in xy3coords: 
-    Sprite(points, ((25*(i[0]+20)),(25*(i[1]+10))))
+    Sprite(points, ((25*(i[0]+(2*a)+(2*b)+6)),(25*(i[1]+10))))
 
 myapp = App()
-myapp.run()
+myapp.run()  
